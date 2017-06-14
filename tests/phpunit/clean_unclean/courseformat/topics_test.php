0b20a4f5d9f242caa2d29a71756a71147e9b9be7
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-06-14 17:00:19 +1000

    Refactored topics format test.

--- tests/phpunit/clean_unclean/courseformat/topics_test.php
+++ tests/phpunit/clean_unclean/courseformat/topics_test.php
@@ -29,14 +29,14 @@ defined('MOODLE_INTERNAL') || die();
 require_once(__DIR__ . '/../../cleanurls_testcase.php');
 
 /**
- * Tests for local_cleanurls_topics.
+ * Tests.
  *
  * @package     local_cleanurls
  * @author      Daniel Thee Roperto <daniel.roperto@catalyst-au.net>
  * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class local_cleanurls_topics_test extends local_cleanurls_testcase {
+class local_cleanurls_topics_cleanunclean_test extends local_cleanurls_testcase {
     public function test_it_supports_topics_format() {
         global $DB;
 
