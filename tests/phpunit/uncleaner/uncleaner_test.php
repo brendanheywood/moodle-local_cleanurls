88d2dfa78cdd079ecad93fba4c4558fdfc79f04a
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-06-14 16:57:21 +1000

    Renamed uncleaner test.

--- tests/phpunit/uncleaner/uncleaner_test.php
+++ tests/phpunit/uncleaner/uncleaner_test.php
@@ -37,7 +37,7 @@ require_once(__DIR__ . '/../cleanurls_testcase.php');
  * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class local_cleanurls_urlparser_test extends local_cleanurls_testcase {
+class local_cleanurls_uncleaner_test extends local_cleanurls_testcase {
     public function test_it_exists() {
         self::assertTrue(class_exists('\local_cleanurls\local\uncleaner\uncleaner'));
     }
