7b957945b0ad0ba15d7a9661fdd4c07a200db000
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-06-14 16:59:15 +1000

    Fixed social uncleaner format test.

--- tests/phpunit/clean_unclean/courseformat/social_test.php
+++ tests/phpunit/clean_unclean/courseformat/social_test.php
@@ -27,14 +27,14 @@ defined('MOODLE_INTERNAL') || die();
 require_once(__DIR__ . '/../../cleanurls_testcase.php');
 
 /**
- * Tests cleaning/uncleaning course section activities.
+ * Tests.
  *
  * @package     local_cleanurls
  * @author      Daniel Thee Roperto <daniel.roperto@catalyst-au.net>
  * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class local_cleanurls_social_test extends local_cleanurls_testcase {
+class local_cleanurls_social_cleanunclean_test extends local_cleanurls_testcase {
     public function test_it_supports_social_format() {
         // No special handling for those URLs as it uses the course URL already.
 
