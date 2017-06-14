a68533d24df148c9179baf2f7d1e5ea49745eea1
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-06-14 17:10:56 +1000

    Refactores user forum test.

--- tests/phpunit/clean_unclean/user_forum_test.php
+++ tests/phpunit/clean_unclean/user_forum_test.php
@@ -21,10 +21,6 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-use local_cleanurls\local\uncleaner\root_uncleaner;
-use local_cleanurls\local\uncleaner\user_forum_uncleaner;
-use local_cleanurls\local\uncleaner\user_uncleaner;
-
 defined('MOODLE_INTERNAL') || die();
 require_once(__DIR__ . '/../cleanurls_testcase.php');
 
@@ -36,25 +32,20 @@ require_once(__DIR__ . '/../cleanurls_testcase.php');
  * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class local_cleanurls_user_forum_uncleaner_test extends local_cleanurls_testcase {
-    public function test_it_can_be_created_in_user_uncleaner() {
-        $root = new root_uncleaner('/user/username/forum');
+class local_cleanurls_user_forum_cleanunclean_test extends local_cleanurls_testcase {
+    public function test_it_cleans_username() {
+        $user = $this->getDataGenerator()->create_user(
+            ['email' => 'someone@example.com', 'username' => 'theusername']);
 
-        $user = $root->get_child();
-        self::assertInstanceOf(user_uncleaner::class, $user);
-
-        self::assertTrue(user_forum_uncleaner::can_create($user));
+        static::assert_clean_unclean('http://www.example.com/moodle/mod/forum/user.php?mode=discussions&id=' . $user->id,
+                                     'http://www.example.com/moodle/user/theusername/discussions');
     }
 
-    public function test_it_cannot_have_unexpected_parent() {
-        $parent = new local_cleanurls_unittest_uncleaner();
-        self::assertFalse(user_forum_uncleaner::can_create($parent));
-    }
+    public function test_it_cleans_username_in_forum_discussion() {
+        $user = $this->getDataGenerator()->create_user(
+            ['email' => 'someone@example.com', 'username' => 'theusername']);
 
-    public function test_it_recognizes_the_path() {
-        $root = new root_uncleaner('/user/username/mypath');
-        $userforum = $root->get_child()->get_child();
-        self::assertInstanceOf(user_forum_uncleaner::class, $userforum);
-        self::assertSame('mypath', $userforum->get_mypath());
+        static::assert_clean_unclean('http://www.example.com/moodle/mod/forum/user.php?mode=discussions&id=' . $user->id,
+                                     'http://www.example.com/moodle/user/theusername/discussions');
     }
 }
